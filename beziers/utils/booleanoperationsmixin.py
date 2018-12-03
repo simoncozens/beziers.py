d1c65e11487ee36289b644a75cfe50adfbba78c5
Simon Cozens <simon@simon-cozens.org>
2018-12-03 15:15:16 +0000

    BOOLEAN CLIPPING OH YES

--- beziers/utils/booleanoperationsmixin.py
+++ beziers/utils/booleanoperationsmixin.py
@@ -3,12 +3,6 @@ from beziers.path.representations.Segment import SegmentRepresentation
 from beziers.utils.intersectionsmixin import Intersection
 
 class BooleanOperationsMixin:
-  booleanOperators = {
-    'unite': { '1': True, '2': True },
-    'intersect': { '2': True },
-    'subtract':  { '1': True },
-    'exclude':   { '1': True, '-1': True }
-  }
 
   def getSelfIntersections(self):
     """Returns a list of a path's self-intersections as Intersection objects."""
@@ -80,3 +74,129 @@ class BooleanOperationsMixin:
         seg = seg.next
 
     self.activeRepresentation = SegmentRepresentation(self,newsegs)
+
+  def clip(self,clip,s_entry,c_entry):
+    splitlist1 = []
+    splitlist2 = []
+    intersections = {}
+    cloned = self.clone()
+    clip = clip.clone()
+
+    for s1 in self.asSegments():
+      for s2 in clip.asSegments():
+        for i in s1.intersections(s2):
+          if i.t1 > 1e-2 and i.t1 < 1-1e-2:
+            if i.seg1 == s1:
+              splitlist1.append((i.seg1,i.t1))
+              splitlist2.append((i.seg2,i.t2))
+            else:
+              splitlist2.append((i.seg1,i.t1))
+              splitlist1.append((i.seg2,i.t2))
+            intersections[i.point] = i
+
+    # print("Split list: %s" % splitlist1)
+    # print("Split list 2: %s" % splitlist2)
+    cloned.splitAtPoints(splitlist1)
+    clip.splitAtPoints(splitlist2)
+    # print("Self:")
+    # print(cloned.asSegments())
+    # print("Clip:")
+    # print(clip.asSegments())
+
+    # print("List of intersections")
+    # for i in intersections.keys(): print(i)
+    def markIntersections(segs,toggle):
+      for s1 in segs:
+        isInter = False
+        for i in intersections.keys():
+          # print("Intersection %s " % intersections[i].point)
+          if i.distanceFrom(s1.start) <= 1:
+            # print("Found intersection at %s" % s1.start)
+            s1.startIsEntry = toggle
+            toggle = not toggle
+            isInter = intersections[i]
+            break
+        s1.startIsIntersection = isInter
+      for i in range(0,len(segs)):
+        seg = segs[i]
+        seg.startIsProcessed = False
+        seg.prev = segs[i-1]
+        if i < len(segs)-1:
+          seg.next = segs[i+1]
+        else:
+          seg.next = segs[0]
+
+    segs1 = cloned.asSegments()
+    segs2 = clip.asSegments()
+    s_entry ^= not clip.pointIsInside(segs1[0].start)
+    markIntersections(segs1, s_entry)
+    c_entry ^= not cloned.pointIsInside(segs2[0].start)
+    markIntersections(segs2, c_entry)
+
+    # Find neighbours
+    for s1 in segs1:
+      if s1.startIsIntersection:
+        for s2 in segs2:
+          if s2.start.distanceFrom(s1.start) <= 1:
+            # print("Intersection in %s " % s1)
+            s1.neighbour = s2
+            s2.neighbour = s1
+
+    shapes = []
+    clipped = []
+
+    def unprocessed(segs):
+      for v in segs:
+        if v.startIsIntersection and not v.startIsProcessed:
+          return True
+      return False
+
+    while unprocessed(segs1):
+      current = segs1[0]
+      while not(current.startIsIntersection) or current.startIsProcessed: current = current.next
+      clipped = []
+      # print("New shape")
+      while True:
+        # print("At intersection: %s" % current)
+        # print(current, current.startIsProcessed, current.startIsIntersection)
+        current.startIsProcessed = True
+        if current.startIsIntersection:
+          current.neighbour.startIsProcessed = True
+        if current.startIsEntry:
+          while True:
+            clipped.append(current.clone())
+            current = current.next
+            # print("Adding segment %s" % current)
+            if current.startIsIntersection:
+              break
+        else:
+          while True:
+            # print("Running backwards")
+            current = current.prev
+            clipped.append(current.reversed())
+            # print("Adding segment %s" % current.reversed())
+            if current.startIsIntersection:
+              break
+        # print("Going to neighbour")
+        current = current.neighbour
+        if current.startIsProcessed:
+          break
+      # print("Final shape: %s" % clipped)
+      shapes.append(clipped)
+    if not shapes:
+      return [ self ]
+    from beziers.path import BezierPath
+    paths = [BezierPath.fromSegments(p) for p in shapes]
+    return paths
+
+  def union(self,other):
+    """Returns a list of Bezier paths representing the union of the two input paths."""
+    return self.clip(other, True, True)
+
+  def intersection(self,other):
+    """Returns a list of Bezier paths representing the intersection of the two input paths."""
+    return self.clip(other, False, False)
+
+  def difference(self,other):
+    """Returns a list of Bezier paths representing the first input path subtracted from the second."""
+    return self.clip(other, True, False)
