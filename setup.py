0f3e90604a50ef4321066a05296f889a903c566b
David Brooks <dave@bcs.co.nz>
2020-12-17 09:57:43 +1300

    Update version for testing.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'description': 'Bezier curve manipulation library',
     'long_description': open('README.rst', 'r').read(),
     'license': 'MIT',
-    'version': '0.1.0',
+    'version': '0.1.1.b1',
     'install_requires': [
         'pyclipper'
     ],
